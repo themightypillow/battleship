3ac45f6e62a464e5aecc54bfb5d320c73ac77d20
Cheri <mightypillow@tutanota.com>
2021-07-14 00:04:15 -0400

    Write and pass first Gameboard test

---
+++ src/Gameboard.js
@@ -0,0 +1,7 @@
+const Gameboard = () => {
+  return {
+
+  };
+};
+
+export default Gameboard;
---
+++ tests/Gameboard.test.js
@@ -0,0 +1,9 @@
+import Gameboard from "../src/Gameboard";
+
+describe('Gameboard', () => {
+  test('create gameboard', () => {
+    expect(Object.keys(Gameboard())).toEqual([
+
+    ]);
+  });
+});
