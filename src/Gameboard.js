dc3d7f522476caf18e0a107b0222afdd8b52bac7
Cheri <mightypillow@tutanota.com>
2021-07-14 13:44:37 -0400

    Pass all Gameboard tests

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -11,46 +11,61 @@ const Gameboard = (rows, columns) => {
       };
     }
   }
+  const ships = [];
 
   const place = (row1, col1, row2, col2) => {
-    if(row1 !== row2 && col1 !== col2) {
-      throw new Error('Cannot place diagonal ship');
-    }
-    else if(row1 !== row2) {
+    if(row1 !== row2 && col1 !== col2) throw new Error('Cannot place diagonal ship');
+    if(row1 !== row2) {
       const min = Math.min(row1, row2);
       const max = Math.max(row1, row2)
       const ship = Ship(max - min + 1);
-      const boardCopy = board.slice();
-      for(let n = min; n <= max; n++) {
+      const boardCopy = board.slice(0);
+      for(let n = min, i = 0; n <= max; n++, i++) {
         if(boardCopy[n][col1].ship) throw new Error('Ship overlaps with another');
         boardCopy[n][col1].ship = ship;
+        boardCopy[n][col1].index = i;
       }
       board = boardCopy;
+      ships.push(ship);
     }
     else if(col1 !== col2) {
       const min = Math.min(col1, col2);
       const max = Math.max(col1, col2);
       const ship = Ship(max - min + 1);
-      const boardCopy = board.slice();
-      for(let n = min; n <= max; n++) {
+      const boardCopy = board.slice(0);
+      for(let n = min, i = 0; n <= max; n++, i++) {
         if(boardCopy[row1][n].ship) throw new Error('Ship overlaps with another');
         boardCopy[row1][n].ship = ship;
+        boardCopy[row1][n].index = i;
       }
       board = boardCopy;
+      ships.push(ship);
     }
     else {
-      // single
+      if(board[row1][col1].ship) throw new Error('Ship overlaps with another');
+      const ship = Ship(1);
+      board[row1][col1].ship = ship;
+      board[row1][col1].index = 0;
+      ships.push(ship);
     }
     return true;
   };
-  const receiveAttack = () => {
-
+  const receiveAttack = (row, column) => {
+    if(board[row][column].attacked) throw new Error('Space already attacked');
+    board[row][column].attacked = true;
+    if(board[row][column].ship) {
+      board[row][column].ship.hit(board[row][column].index);
+      return true;
+    }
+    else {
+      return false;
+    }
   };
   const haveAllSunk = () => {
-
+    return ships.every(ship => ship.isSunk());
   };
   const get = () => {
-
+    return board.slice(0);
   };
   return {
     place,
--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -22,74 +22,80 @@ describe('Gameboard', () => {
     board.place(0, 0, 1, 0);
     expect(() => board.place(0, 0, 0, 1)).toThrow('Ship overlaps with another');
   });
-  xtest('make a miss', () => {
+  test('make a miss', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     expect(board.receiveAttack(0, 1)).toBe(false);
   });
-  xtest('make a hit', () => {
+  test('make a hit', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     expect(board.receiveAttack(1, 0)).toBe(true);
   });
-  xtest('all ships have not been sunk', () => {
+  test('all ships have not been sunk', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     board.receiveAttack(0, 0);
     expect(board.haveAllSunk()).toBe(false);
   });
-  xtest('all ships have been sunk', () => {
+  test('all ships have been sunk', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     board.receiveAttack(0, 0);
     board.receiveAttack(1, 0);
     expect(board.haveAllSunk()).toBe(true);
   });
-  xtest('empty board is represented', () => {
+  test('empty board is represented', () => {
     const board = Gameboard(2, 2);
     expect(board.get()).toEqual([
       [{ attacked: false, ship: null }, { attacked: false, ship: null }],
       [{ attacked: false, ship: null }, { attacked: false, ship: null }]
     ]);
   });
-  xtest('board with vertical ship is represented', () => {
+  test('board with vertical ship is represented', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     const boardArray = board.get();
-    expect(boardArray[0][0].ship).toBeNull();
-    expect(boardArray[1][0].ship).toBeNull();
+    expect(boardArray[0][0].ship).not.toBeNull();
+    expect(boardArray[1][0].ship).not.toBeNull();
   });
-  xtest('board with vertical ship with no order is represented', () => {
+  test('board with vertical ship with no order is represented', () => {
     const board = Gameboard(2, 2);
     board.place(1, 0, 0, 0);
     const boardArray = board.get();
-    expect(boardArray[0][0].ship).toBeNull();
-    expect(boardArray[1][0].ship).toBeNull();
+    expect(boardArray[0][0].ship).not.toBeNull();
+    expect(boardArray[1][0].ship).not.toBeNull();
   });
-  xtest('board with horizontal ship is represented', () => {
+  test('board with horizontal ship is represented', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 0, 1);
     const boardArray = board.get();
-    expect(boardArray[0][0].ship).toBeNull();
-    expect(boardArray[0][1].ship).toBeNull();
+    expect(boardArray[0][0].ship).not.toBeNull();
+    expect(boardArray[0][1].ship).not.toBeNull();
   });
-  xtest('board with horizontal ship with no order is represented', () => {
+  test('board with horizontal ship with no order is represented', () => {
     const board = Gameboard(2, 2);
     board.place(0, 1, 0, 0);
     const boardArray = board.get();
-    expect(boardArray[0][0].ship).toBeNull();
-    expect(boardArray[0][1].ship).toBeNull();
+    expect(boardArray[0][0].ship).not.toBeNull();
+    expect(boardArray[0][1].ship).not.toBeNull();
   });
-  xtest('board with missed attack is represented', () => {
+  test('board with missed attack is represented', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     board.receiveAttack(0, 1);
     expect(board.get()[0][1].attacked).toBe(true);
   });
-  xtest('board with hit attack is represented', () => {
+  test('board with hit attack is represented', () => {
     const board = Gameboard(2, 2);
     board.place(0, 0, 1, 0);
     board.receiveAttack(0, 0);
     expect(board.get()[0][0].attacked).toBe(true);
+  });
+  test('cannot attack the same spot twice', () => {
+    const board = Gameboard(2, 2);
+    board.place(0, 0, 1, 0);
+    board.receiveAttack(0, 0);
+    expect(() => board.receiveAttack(0, 0)).toThrow('Space already attacked');
   });
 });
