5dd44e7f1b68492992ee8c81ff2e93bfa7337787
Cheri <mightypillow@tutanota.com>
2021-07-14 16:04:48 -0400

    Pass human player tests

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,22 @@
-const Player = () => {
+const Player = (board, rows, columns, isHuman) => {
+  const plays = [];
 
+  const play = (row, column) => {
+    if(isHuman) {
+      if(row >= rows || column >= columns) throw new Error('Space out of bounds');
+      board.receiveAttack(row, column);
+      return {
+        row,
+        column
+      };
+    }
+    else {
+
+    }
+  };
+  return {
+    play
+  };
 };
 
 export default Player;
