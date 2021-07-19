1f93dc93e84714f53af095f745f26388c3698588
Cheri <mightypillow@tutanota.com>
2021-07-18 21:13:13 -0400

    Pass Game tests

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,41 @@
-const Game = (() => {
+import Gameboard from './Gameboard';
+import Player from './Player';
+import { defaultShips } from '../tests/data/default-board';
 
+const Game = (() => {
+  const userBoard = Gameboard(10, 10);
+  const cpuBoard = Gameboard(10, 10);
+  const user = Player(cpuBoard, 10, true);
+  const cpu = Player(userBoard, 10, false);
+
+  const placeShips = (userShips) => {
+    userShips.forEach(location => userBoard.place(
+      location.startRow, location.startColumn, location.endRow, location.endColumn
+    ));
+    defaultShips.forEach(location => cpuBoard.place(
+      location.startRow, location.startColumn, location.endRow, location.endColumn
+    ));
+  };
+
+  const makeUserTurn = (row, column) => {
+    return {
+      hit: user.play(row, column).hit,
+      win: cpuBoard.haveAllSunk()
+    };
+  };
+
+  const makeCPUTurn = () => {
+    return {
+      ...cpu.play(),
+      win: userBoard.haveAllSunk(),
+    };
+  };
+
+  return {
+    placeShips,
+    makeUserTurn,
+    makeCPUTurn
+  };
 })();
 
 export default Game;
--- tests/Game.test.js
+++ tests/Game.test.js
@@ -10,21 +10,21 @@ describe('Game Loop', () => {
       win: false
     });    
   });
-  xtest('cpu makes a turn', () => {
+  test('cpu makes a turn', () => {
     const move = Game.makeCPUTurn();
-    expect(move.win).toBe(false);
     expect(move.hit).toBeDefined();
     expect(move.row).toBeDefined();
     expect(move.column).toBeDefined();
+    expect(move.win).toBe(false);
   });
-  xtest('user makes a turn, hit', () => {
+  test('user makes a turn, hit', () => {
     let first = defaultCoordinates.shift();
     expect(Game.makeUserTurn(first.row, first.column)).toEqual({
       hit: true,
       win: false
     });
   });
-  xtest('game ends after user hits all ships', () => {
+  test('game ends after user hits all ships', () => {
     const last = defaultCoordinates.pop();
     defaultCoordinates.forEach(coords => Game.makeUserTurn(coords.row, coords.column));
     expect(Game.makeUserTurn(last.row, last.column)).toEqual({
