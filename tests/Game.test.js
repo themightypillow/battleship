b165fc6da5884521cd75df383d41e0c08cae6192
Cheri <mightypillow@tutanota.com>
2021-07-18 20:54:27 -0400

    Rewrite game tests

--- tests/Game.test.js
+++ tests/Game.test.js
@@ -1,36 +1,34 @@
 import Game from '../src/Game';
-import { defaultShips } from './data/default-board';
+import { defaultShips, defaultCoordinates } from './data/default-board';
 
 describe('Game Loop', () => { 
-  test('game should start with no errors', () => {
-    expect(() => Game.start(defaultShips)).not.toThrow();
-  });  
-  xtest('game not over in beginning', () => {
-    expect(Game.isOver()).toBe(false);
+  Game.placeShips(defaultShips);
+
+  test('user makes a turn, miss', () => {
+    expect(Game.makeUserTurn({ row: 0, column: 0 })).toEqual({
+      hit: false,
+      win: false
+    });    
   });
-  xtest('computer attacks one after player attacks', () => {
-    expect(Game.attack(1, 0).reduce((row, sum1) => {
-      row.reduce((space, sum2) => space.attacked ? ++sum2 : sum2, 0);
-      return sum1 + sum2;
-    }, 0)).toBe(1);
+  test('cpu makes a turn', () => {
+    const move = Game.makeCPUTurn();
+    expect(move.win).toBe(false);
+    expect(move.hit).toBeDefined();
+    expect(move.row).toBeDefined();
+    expect(move.column).toBeDefined();
   });
-  xtest('game not over after one set of attacks', () => {
-    expect(Game.isOver()).toBe(false);
+  test('user makes a turn, hit', () => {
+    expect(Game.makeUserTurn(defaultCoordinates.shift())).toEqual({
+      hit: true,
+      win: false
+    });
   });
-  xtest('two computer attacks have occurred', () => {
-    expect(Game.attack(1, 0).reduce((row, sum1) => {
-      row.reduce((space, sum2) => space.attacked ? ++sum2 : sum2, 0);
-      return sum1 + sum2;
-    }, 0)).toBe(2);
-  });
-  xtest('game reaches an end', () => {
-    for(let r = 0; r < 10; r++) {
-      for(let c = 0; c < 10; c++) {
-        if(Game.isOver()) break;
-        if((r === 0 && c === 0) || (r === 1 && c === 0)) continue;
-        Game.attack(r, c);
-      }
-    }
-    expect(Game.isOver()).toBe(true);
+  test('game ends after user hits all ships', () => {
+    const last = defaultCoordinates.pop();
+    defaultCoordinates.forEach(coords => Game.makeUserTurn(coords));
+    expect(Game.makeUserTurn(last)).toEqual({
+      hit: true,
+      win: true
+    });
   });
 });
--- tests/data/default-board.js
+++ tests/data/default-board.js
@@ -1,9 +1,29 @@
 export const defaultShips = [
-  { startRow: 4, startColumn: 2, endRow: 8, endColumn: 2},
-  { startRow: 1, startColumn: 1, endRow: 1, endColumn: 4},
-  { startRow: 4, startColumn: 6, endRow: 4, endColumn: 8},
-  { startRow: 7, startColumn: 7, endRow: 9, endColumn: 7},
-  { startRow: 6, startColumn: 0, endRow: 7, endColumn: 0}
+  { startRow: 4, startColumn: 2, endRow: 8, endColumn: 2 },
+  { startRow: 1, startColumn: 1, endRow: 1, endColumn: 4 },
+  { startRow: 4, startColumn: 6, endRow: 4, endColumn: 8 },
+  { startRow: 7, startColumn: 7, endRow: 9, endColumn: 7 },
+  { startRow: 6, startColumn: 0, endRow: 7, endColumn: 0 }
+];
+
+export const defaultCoordinates = [
+  { row: 4, column: 2 },
+  { row: 5, column: 2 },
+  { row: 6, column: 2 },
+  { row: 7, column: 2 },
+  { row: 8, column: 2 },
+  { row: 1, column: 1 },
+  { row: 1, column: 2 },
+  { row: 1, column: 3 },
+  { row: 1, column: 4 },
+  { row: 4, column: 6 },
+  { row: 4, column: 7 },
+  { row: 4, column: 8 },
+  { row: 7, column: 7 },
+  { row: 8, column: 7 },
+  { row: 9, column: 7 },
+  { row: 6, column: 0 },
+  { row: 7, column: 0 }
 ];
 
 export const defaultRender = [
